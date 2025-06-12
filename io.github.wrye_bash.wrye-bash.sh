6de7787b497c5f4657415190ee9e0503cc94e710
Alice H. <alice.hall0451+github@gmail.com>
2025-06-12 07:34:08 +0100

    update manifest, sample config and patches
    * Add missing p7zip dependency
    * Add patch to read bash.ini from XDG_CONFIG_HOME
    * Add sample boot-settings.toml
    * Copy bash_default.ini and boot-settings_default.toml if not exist
    * Add patch to skip locale files backup
    * Change XDG_DATA_HOME path to wrye-bash for consistency
    
    Signed-off-by: Alice H. <alice.hall0451+github@gmail.com>

--- io.github.wrye_bash.wrye-bash.sh
+++ io.github.wrye_bash.wrye-bash.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+[ ! -e "$XDG_CONFIG_HOME"/bash.ini ] &&
+	install -Dm644 /app/share/wrye-bash/bash_default.ini "$XDG_CONFIG_HOME"/bash.ini
+[ ! -e "$XDG_CONFIG_HOME"/boot-settings.toml ] &&
+	install -Dm644 /app/share/wrye-bash/boot-settings_default.toml "$XDG_CONFIG_HOME"/boot-settings.toml
+
 python "/app/share/wrye-bash/Wrye Bash Launcher.pyw" "$@"
