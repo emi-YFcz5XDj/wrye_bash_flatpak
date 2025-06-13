62313f44a1fea23f6d499931de304c95a877abdd
Alice H. <alice.hall0451+github@gmail.com>
2025-06-13 02:47:01 +0100

    update manifest and script, rename patches
    * Remove xdg-data/Steam:rw permission, bwrap follows symlink
    * Move NVIDIA workaround variable to script to export it conditionally
    * Rename patches
    
    Signed-off-by: Alice H. <alice.hall0451+github@gmail.com>

--- io.github.wrye_bash.wrye-bash.sh
+++ io.github.wrye_bash.wrye-bash.sh
@@ -4,4 +4,6 @@
 [ ! -e "$XDG_CONFIG_HOME"/boot-settings.toml ] &&
 	install -Dm644 /app/share/wrye-bash/boot-settings_default.toml "$XDG_CONFIG_HOME"/boot-settings.toml
 
-python "/app/share/wrye-bash/Wrye Bash Launcher.pyw" "$@"
+# Workaround NVIDIA bug https://bugs.webkit.org/show_bug.cgi?id=259644
+grep -q org.freedesktop.Platform.GL.nvidia /.flatpak-info && export WEBKIT_DISABLE_DMABUF_RENDERER=1
+exec python "/app/share/wrye-bash/Wrye Bash Launcher.pyw" "$@"
