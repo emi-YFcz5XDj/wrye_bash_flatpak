70a51424b97cfd080727a7992dc7d016f0f9c7cf
Alice H. <alice.hall0451+vcs@gmail.com>
2025-10-26 17:29:52 +0000

    update script
    Signed-off-by: Alice H. <alice.hall0451+vcs@gmail.com>

--- io.github.wrye_bash.wrye-bash.sh
+++ io.github.wrye_bash.wrye-bash.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-[ ! -e "$XDG_CONFIG_HOME"/bash.ini ] &&
-	install -Dm644 /app/share/wrye-bash/bash_default.ini "$XDG_CONFIG_HOME"/bash.ini
-[ ! -e "$XDG_CONFIG_HOME"/boot-settings.toml ] &&
-	install -Dm644 /app/share/wrye-bash/boot-settings_default.toml "$XDG_CONFIG_HOME"/boot-settings.toml
+[ ! -e "${XDG_CONFIG_HOME}/bash.ini" ] &&
+	install -Dm644 /app/share/wrye-bash/bash_default.ini "${XDG_CONFIG_HOME}/bash.ini"
+[ ! -e "${XDG_CONFIG_HOME}/boot-settings.toml" ] &&
+	install -Dm644 /app/share/wrye-bash/boot-settings_default.toml "${XDG_CONFIG_HOME}/boot-settings.toml"
 
 # Workaround NVIDIA bug https://bugs.webkit.org/show_bug.cgi?id=259644
 grep -q org.freedesktop.Platform.GL.nvidia /.flatpak-info && export WEBKIT_DISABLE_DMABUF_RENDERER=1
